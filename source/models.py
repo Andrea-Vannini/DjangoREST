d9efabf268341ceca4d41fdc084c566fbb7ae880
Andrea-Vannini <andreavannini1@gmail.com>
2023-03-01 12:02:02 +0100

    Origin model

--- source/models.py
+++ source/models.py
@@ -2,10 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Origin(models.Model):
+	name = models.CharField(max_length=100, unique=False, verbose_name='name')
+	url = models.URLField(max_length=200, unique=False, verbose_name='url')
+
+	class Meta:
+		verbose_name_plural = 'Origins'
+
+	def __str__(self):
+		return self.name
+
+
 class Source(models.Model):
 	name = models.CharField(max_length=100, unique=True, verbose_name='name')
-	url = models.URLField(max_length=200, unique=True, verbose_name='url')
-	description = models.TextField(max_length=1000, blank=True, verbose_name='description')
+	origin = models.ForeignKey(Origin, on_delete=models.CASCADE, default=1)
+	url = models.URLField(max_length=400, unique=False, verbose_name='url')
 
 	class Meta:
 		verbose_name_plural = 'Sources'
